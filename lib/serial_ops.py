2c10d4417fb7d03ea49d88ce499597f5262df33b
yabool2001 <mzemlo.pl@gmail.com>
2022-09-19 22:24:55 +0200

    Initial commit

---
+++ lib/serial_ops.py
@@ -0,0 +1,40 @@
+import platform
+from pprint import pprint
+import serial
+import serial.tools.list_ports
+
+def set_serials_cfg ( com ) :
+    serial_ports =  serial.tools.list_ports.comports()
+    for s_p in serial_ports:
+        #pprint ( s_p.description.lower () )
+        if 'stlink'.lower () in s_p.description.lower () :
+            if platform.system () == "Windows":
+                com.port = s_p.name
+            elif platform.system () == "Linux":
+                com.port = '/dev/' + s_p.name
+            else:
+                print ( f"Error: No compatible os!" )
+    com.baudrate       = 115200
+    com.bytesize       = serial.EIGHTBITS
+    com.parity         = serial.PARITY_NONE
+    com.stopbits       = serial.STOPBITS_ONE
+    com.timeout        = 0.15
+    com.write_timeout  = 0.15
+
+
+def close_serial_ports ( *sp ) :
+    for i in sp :
+        if i.is_open :
+            try:
+                i.close ()
+                print ( f"{i.name} port is closed" )
+            except serial.SerialException as e :
+                print ( f"{i.name} port closing error: {str(e)}" )
+
+def open_serial_ports ( *sp ) :
+    for i in sp :
+        try: 
+            i.open ()
+            print ( f"{i.name} port opened" )
+        except serial.SerialException as e :
+            print ( f"{i.name} port error opening: {str(e)}" )
---
+++ main.py
@@ -0,0 +1,33 @@
+import datetime
+from pprint import pprint
+import serial
+import serial.tools.list_ports
+from lib.serial_ops import open_serial_ports, set_serials_cfg , close_serial_ports , open_serial_ports
+
+data_com_delta_seconds          = 60*60*24
+
+gn_mostrecent_at_comm	        = b'$GN @*67\n'
+error_at_answer                 = b'$CMD ERR,CMD_NOTIMPLEMENTED*0d\n' 
+gn_mostrecent_answer	        = b'$GN 52.2782,20.8089,84,359,2*2b\n'
+
+at_com = serial.Serial ()
+set_serials_cfg ( at_com )
+open_serial_ports ( at_com )
+
+at_com.reset_input_buffer()
+at_com.reset_output_buffer()
+
+#frame = at_com.read ( 4666 )
+min = datetime.datetime.utcnow().minute
+frame_read_time_up = datetime.datetime.utcnow () + datetime.timedelta ( seconds = data_com_delta_seconds )
+while datetime.datetime.utcnow () < frame_read_time_up :
+    frame = at_com.read ( 100 )
+    if frame == gn_mostrecent_at_comm :
+        at_com.write ( gn_mostrecent_answer )
+    if frame != b'' :
+        pprint ( frame )
+    if min != datetime.datetime.utcnow().minute :
+        at_com.write ( error_at_answer )
+        min = datetime.datetime.utcnow().minute
+
+close_serial_ports ( at_com )
