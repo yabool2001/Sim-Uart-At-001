4e00e82732ec85971b891038e0541b39593b8053
yabool2001 <mzemlo.pl@gmail.com>
2022-09-20 21:11:28 +0200

    aaaa

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+from errno import EOVERFLOW
 from pprint import pprint
 import serial
 import serial.tools.list_ports
@@ -9,6 +10,7 @@ data_com_delta_seconds          = 60*60*24
 gn_mostrecent_at_comm	        = b'$GN @*67\n'
 error_at_answer                 = b'$CMD ERR,CMD_NOTIMPLEMENTED*0d\n' 
 gn_mostrecent_answer	        = b'$GN 52.2782,20.8089,84,359,2*2b\n'
+eol                 	        = b'\n'
 
 at_com = serial.Serial ()
 set_serials_cfg ( at_com )
@@ -27,7 +29,9 @@ while datetime.datetime.utcnow () < frame_read_time_up :
     if frame != b'' :
         pprint ( frame )
     if min != datetime.datetime.utcnow().minute :
-        at_com.write ( error_at_answer )
+        #at_com.write ( error_at_answer )
+        current_answer = f"{datetime.datetime.utcnow ().strftime('$GN %Y,%M,%D %H.%M.%S*2b')}{eol}"
+        at_com.write ( current_answer.encode('utf-8') )
         min = datetime.datetime.utcnow().minute
 
 close_serial_ports ( at_com )
