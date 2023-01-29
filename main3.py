f2f5e68d7cf3b615bfd523c294ba034340cc513c
yabool2001 <mzemlo.pl@gmail.com>
2023-01-29 17:31:15 +0100

    aaaa

--- main3.py
+++ main3.py
@@ -1,3 +1,4 @@
+import datetime
 from pprint import pprint
 import random
 import serial
@@ -7,9 +8,10 @@ from lib.serial_ops import open_serial_ports, set_serials_cfg , close_serial_por
 com_name                        = "stlink"
 #com_name                        = "cp210x"
 stm32_request                   = "request"
+stm32_dbg                       = "DBG: "
 python_answer                   = "answer answer"
 error                           = b'$CMD ERR,CMD_NOTIMPLEMENTED*0d\n' 
-p                               = 0
+eol                 	        = " "
 
 at_com = serial.Serial ()
 set_serials_cfg ( at_com , com_name )
@@ -19,17 +21,23 @@ at_com.reset_input_buffer()
 at_com.reset_output_buffer()
 
 while True :
-    rand1 = random.randrange ( 1 , 2 ) 
     frame = at_com.read ( 100 )
     if frame.decode('utf-8')[:7] == stm32_request :
-        print ( f"STM32 sent AT command: {frame.decode('utf-8')}" )
+        ts = f"{datetime.datetime.utcnow ().strftime('%H:%M:%S.%f')}{eol}"
+        print ( f"{ts}1. STM32 sent AT command: {frame.decode('utf-8')}" )
         answer = f"{python_answer}"
         at_com.write ( answer.encode ( 'utf-8' ) )
-        print ( f"Python sent answer: {answer.encode ( 'utf-8' )}" )
-    if frame.decode('utf-8')[:6] == python_answer :
-        print ( f"***STM32 transfer python answer: {frame.decode('utf-8')}" )
-    if rand1 == random.randrange ( 1 , 2 )  :
+        ts = f"{datetime.datetime.utcnow ().strftime('%H:%M:%S.%f')}{eol}"
+        print ( f"{ts}2. Python sent answer: {answer.encode ( 'utf-8' )}" )
+    if frame.decode('utf-8')[:6] == python_answer[:6] :
+        ts = f"{datetime.datetime.utcnow ().strftime('%H:%M:%S.%f')}{eol}"
+        print ( f"{ts}3. ***STM32 transfer python answer: {frame.decode('utf-8')}" )
+    if frame.decode('utf-8')[:5] == stm32_dbg :
+        ts = f"{datetime.datetime.utcnow ().strftime('%H:%M:%S.%f')}{eol}"
+        print ( f"{ts}STM32 {frame.decode('utf-8')}" )
+    if random.randrange ( 1 , 2000 ) == 1 :
+        ts = f"{datetime.datetime.utcnow ().strftime('%H:%M:%S.%f')}{eol}"
         at_com.write ( error )
-        print ( f"Python sent error: {error}" )
+        print ( f"{ts}Python sent error: {error}" )
 
 close_serial_ports ( at_com )
